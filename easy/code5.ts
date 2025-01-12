bf2e2c5abdfec0f33504ac14ad055f4a0ce5d5dd
DavideAvagnano <dav.avagnano@gmail.com>
2025-01-12 10:16:41 +0100

    easy: MERGE TWO SORTED LISTS code5

---
+++ easy/code5.ts
@@ -0,0 +1,108 @@
+/*
+MERGE TWO SORTED LISTS
+You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+Return the head of the merged linked list.
+
+Example 1:
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+Input: list1 = [], list2 = []
+Output: []
+
+Example 3:
+Input: list1 = [], list2 = [0]
+Output: [0]
+
+Constraints:
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+*/
+
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null {
+  if (!list1) return list2;
+  if (!list2) return list1;
+
+  const dummy = new ListNode(-1);
+  let current = dummy;
+
+  while (list1 !== null && list2 !== null) {
+    if (list1.val <= list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+
+  if (list1 !== null) current.next = list1;
+  if (list2 !== null) current.next = list2;
+
+  return dummy.next;
+}
+// O(n+m) --> O(n)
+// O(1)
+
+// TEST Input: list1 = [1,2,4], list2 = [1,3,4]
+const list1 = new ListNode(1);
+list1.next = new ListNode(2);
+list1.next.next = new ListNode(4);
+
+const list2 = new ListNode(1);
+list2.next = new ListNode(3);
+list2.next.next = new ListNode(4);
+
+const mergedList = mergeTwoLists(list1, list2);
+
+let current = mergedList;
+while (current !== null) {
+  console.log(current.val);
+  current = current.next;
+}
+
+// HELPERS
+function createLinkedList(arr: number[]): ListNode | null {
+  if (arr.length === 0) return null;
+  const head = new ListNode(arr[0]);
+  let current = head;
+  for (let i = 1; i < arr.length; i++) {
+    current.next = new ListNode(arr[i]);
+    current = current.next;
+  }
+  return head;
+}
+function linkedListToArray(head: ListNode | null): number[] {
+  const result: number[] = [];
+  let current = head;
+  while (current !== null) {
+    result.push(current.val);
+    current = current.next;
+  }
+  return result;
+}
+
+// TEST
+const newList1 = createLinkedList([1, 2, 4]);
+const newList2 = createLinkedList([1, 3, 4]);
+const newMergedList = mergeTwoLists(newList1, newList2);
+console.log(linkedListToArray(newMergedList));
